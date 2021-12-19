dfab28cdd4f84e3ae5a09d9edf814523fec0a3ae
jungbada269 <57929751+jungbada269@users.noreply.github.com>
2021-12-19 23:48:01 +0900

    데이터 타입 스터디

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,2 +1,24 @@
 console.log("hello javascript!")
-alert("hello")
+// alert("hello")
+
+var name = "scalper"
+
+// for (var i=0; i<10; i++) {
+//     var name = "scalper"
+//     var engName = 'codeScalper'
+// }
+
+console.log(name)
+
+// let, const
+
+
+// String, Number, boolean, null, undefined
+
+const naem = "scalper"
+const age = 30
+const isMale = true
+const isFemael = false
+let money = null;
+let girlfriend = undefined
+
